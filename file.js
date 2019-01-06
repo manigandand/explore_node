9d8031c7071cec91a1397583788830d96fdc4250
Manigandan Dharmalingam <manigandan.jeff@gmail.com>
2019-01-06 18:42:08 +0530

    #10 - Creating & Removing Directories

--- file.js
+++ file.js
@@ -18,3 +18,29 @@ fs.readFile("file.txt", "utf8", function (err, data) {
         console.log('File Saved!');
     });
 });
+
+console.log('----------------------------------------------------------------');
+
+// create dir
+
+fs.mkdir('assets', function () {
+    fs.readFile('file.txt', 'utf8', function (err, data) {
+        if (err) {
+            console.log("Oops, something went wrong ", err);
+            return;
+        }
+
+        fs.writeFile('./assets/file_write.txt', data, function (err) {
+            if (err) throw err;
+            console.log('File Saved!');
+        });
+    });
+});
+
+fs.mkdir('stuff', function (err) {
+    if (err) throw err;
+});
+setTimeout(function () {}, 2000);
+fs.rmdir('stuff', function (err) {
+    if (err) throw err;
+});
