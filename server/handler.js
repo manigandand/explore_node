20ab8290f24bf020491792c695d758b1e766eaa9
Manigandan Dharmalingam <manigandan.jeff@gmail.com>
2019-01-06 23:39:53 +0530

    19 - Basic Routing

---
+++ server/handler.js
@@ -0,0 +1,42 @@
+var fs = require('fs');
+
+// home
+var home = function (res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+
+    var readStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+    readStream.pipe(res);
+};
+
+// api handler
+var apiHandler = function (res) {
+    res.writeHead(200, {
+        'Content-Type': 'application/json'
+    });
+    var response = {
+        "data": {
+            "message": "Hello NodeJS - Up!",
+            "uptime": Date.now()
+        },
+        "meta": {
+            "status": 200
+        }
+    }
+
+    res.end(JSON.stringify(response))
+};
+
+// 404 - page not found
+
+var notFound = function (res) {
+    res.writeHead(404);
+    res.end("The endpoint you're looking for is not found");
+}
+
+module.exports = {
+    home: home,
+    api: apiHandler,
+    notFound: notFound
+}
--- server/server.js
+++ server/server.js
@@ -1,37 +1,20 @@
 var http = require('http');
+var handler = require('./handler')
+
 
 var server = http.createServer(function (req, res) {
     console.log(req.method + ": " + req.url)
-
-    res.writeHead(200, {
-        'Content-Type': 'application/json'
-    });
-    var response = {
-        "data": {
-            "message": "Hello NodeJS"
-        },
-        "meta": {
-            "status": 200
-        }
+    switch (req.url) {
+        case ('/' || '/home'):
+            handler.home(res);
+            break;
+        case '/api/v1/helth-check':
+            handler.api(res);
+            break;
+        default:
+            handler.notFound(res);
     }
-
-    res.end(JSON.stringify(response))
 });
 
 server.listen(3000, '127.0.0.1');
 console.log('listening on :3000');
-
-var fs = require('fs');
-var server2 = http.createServer(function (req, res) {
-    console.log(req.method + ": " + req.url)
-
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-
-    var readStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-    readStream.pipe(res);
-});
-
-server2.listen(3001, '127.0.0.1');
-console.log('HTML server listening on :3001');
