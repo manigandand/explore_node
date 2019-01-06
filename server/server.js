69ae00d29b0ca1afcc0ed049ba9febf703001639
Manigandan Dharmalingam <manigandan.jeff@gmail.com>
2019-01-06 23:14:32 +0530

    serve html file as stream

--- server/server.js
+++ server/server.js
@@ -20,3 +20,18 @@ var server = http.createServer(function (req, res) {
 
 server.listen(3000, '127.0.0.1');
 console.log('listening on :3000');
+
+var fs = require('fs');
+var server2 = http.createServer(function (req, res) {
+    console.log(req.method + ": " + req.url)
+
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+
+    var readStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+    readStream.pipe(res);
+});
+
+server2.listen(3001, '127.0.0.1');
+console.log('HTML server listening on :3001');
